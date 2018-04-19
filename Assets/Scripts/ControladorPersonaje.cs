e22772c5f0460619b1d2238081a896cef1511d32
OscarLopezFabregas <oscar.lopez6@alumni.esade.edu>
2018-04-19 18:52:24 +0200

    added playercontroller and animator

---
+++ Assets/Scripts/ControladorPersonaje.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorPersonaje : MonoBehaviour {
+
+    public float fuerzaSalto = 100f;
+    private Rigidbody2D rb2d;
+    private bool enSuelo = true;
+    public Transform comprobadorSuelo;
+    float comprobadorRadio = 0.07f;
+    public LayerMask mascaraSuelo;
+
+    private bool dobleSalto = false;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    // Use this for initialization
+    void Start ()
+    {
+       
+	}
+
+    private void FixedUpdate()
+    {
+        enSuelo = Physics2D.OverlapCircle(comprobadorSuelo.position, comprobadorRadio, mascaraSuelo);
+        animator.SetBool("isGrounded", enSuelo);
+        if(enSuelo)
+        {
+            dobleSalto = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if ((enSuelo || !dobleSalto) && Input.GetMouseButtonDown(0))
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, fuerzaSalto);
+            //rb2d.AddForce(new Vector2(0, fuerzaSalto));
+            if(!dobleSalto && !enSuelo)
+            {
+                dobleSalto = true;
+            }
+        } 
+	}
+}
